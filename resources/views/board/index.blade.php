b98d796c60bca9f331c8fb940de638f282c44967
lulu1417 <boa1417@gmail.com>
2019-10-17 16:08:19 +0800

    index.blade.php

--- resources/views/board/index.blade.php
+++ resources/views/board/index.blade.php
@@ -1,71 +1,21 @@
-{{--@extends('board.layout')--}}
-{{--@section('content')--}}
-{{--    <div class="flex-center position-ref full-height">--}}
-{{--    <div class="top-right home">--}}
-{{--        <a class="btn btn-success" href="{{ route('board.create') }}">Leave a message</a>--}}
-{{--        <a class="btn btn-success" href="{{ route('board.create') }}">Login</a>--}}
-{{--    </div>--}}
-{{--<div class="note full-height">--}}
-{{--    @foreach ($messages as $message)--}}
-
-{{--        <br>No：{{ ++$i }}</br>--}}
-{{--        <br>Visitor Name：{{ $message->name }}</br>--}}
-{{--        <br>Subject：{{ $message->subject }}</br>--}}
-{{--        <br>Content：{{ $message->content }}</br>--}}
-{{--        @if<br>Image：{{ $product->image }}</br>@endif--}}
-{{--        <br>--}}
-{{--        <form action="{{ route('board.destroy',$message->id) }}" method="POST">--}}
-
-{{--            <a class="btn btn-primary" href="{{ route('board.edit',$message->id) }}">Edit</a>--}}
-{{--            <a class="btn btn-info" href="{{ route('board.destroy',$message->id) }}">Delete</a>--}}
-{{--            @csrf--}}
-{{--            @method('DELETE')--}}
-{{--        </form>--}}
-{{--        </br>--}}
-
-{{--    @endforeach--}}
-{{--    <br>--}}
-{{--    <div class="bottom left position-abs content">--}}
-{{--        There are {{$i}} messages.--}}
-{{--    </div>--}}
-{{--    </br>--}}
-{{--</div>--}}
-
-{{--@endsection--}}
-
-
-@extends('products.layout')
-
+@extends('board.layout')
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
-            </div>
+    <div class="flex-center position-ref full-height">
+        <div class="top-right home">
+            <a class="btn btn-success" href="{{ route('board.create') }}">Leave a message</a>
+            <a class="btn btn-success" href="{{ url('') }}">Login</a>
         </div>
-    </div>
-
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Subject</th>
-            <th>Details</th>
-            <th width="280px">Action</th>
-        </tr>
-        @foreach ($messages as $product)
-            <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->subject }}</td>
-                <td>{{ $product->content }}</td>
-                <td>
+        <div class="note full-height">
+            @foreach ($messages as $product)
+                <tr>
+                    <br>No：
+                    {{ ++$i }}
+                    <br>Name：
+                    {{ $product->name }}
+                    <br>Subject：
+                    {{ $product->subject }}
+                    <br>Details：
+                    {{ $product->content }}
                     <form action="{{ route('board.destroy',$product->id) }}" method="POST">
 
                         <a class="btn btn-info" href="{{ route('board.show',$product->id) }}">Show</a>
@@ -75,13 +25,22 @@
                         @csrf
                         @method('DELETE')
 
-                        <button type="submit" class="btn btn-danger">Delete</button>
+                       <br> <button type="submit" style=" padding:5px 15px;
+                background:#FFCCCC;
+                color: #444444;
+                border:0 none;
+                cursor:pointer;
+                -webkit-border-radius: 5px;
+                border-radius: 5px;
+                font-family: 'Nunito', sans-serif;
+                font-size: 19px;">Delete
+                        </button>
                     </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-
-    {!! $messages->links() !!}
+                </tr>
+                @endforeach
+                </table>
+                <div class="bottom left position-abs content">
+                    There are {{$i}} messages.
+                </div>
 
 @endsection
